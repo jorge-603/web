a1b5151ca9a25d83e39d49e7a8e4e6a9c65bfccc
jorge-603 <jorgean9999@gmail.com>
2024-10-07 00:21:25 -0600

    personal-web

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="es_MX">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/jpg" href="favicon.ico" />
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>Jorge#603</title>
+</head>
+
+<body>
+    <nav class="toolbar">
+        <div class="toolbar-icon-container">
+            <img class="icon" src="assets/img/icon.png" alt="icon-jorge">
+        </div>
+        <div class="toolbar-title-container">
+            <h3 class="title"><a href=".">Jorge#603</a></h1>
+        </div>
+        <ul class="menu">
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#proyectos">Proyectos</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+        </div>
+    </nav>
+
+    <div class="container presentation">
+        <div class="presentation-section">
+            <img class="icon-presentation" src="assets/img/icon.png" alt="icon-jorge">
+            <h1 class="title name-title">Jorge#603</h1>
+            <h2 class="subtitle description-title">Desarrollador Backend</h2>
+            <small class="little-msg">Ingeniero en Sistemas Computacionales (80%)</small><br>
+            <small class="little-msg">SysAdmin en proceso (50%)</small><br>
+            <small class="little-msg">e intento de YouTuber/Streamer (Si%)</small>
+            <div class="links-container">
+                <a href="https://github.com/jorge-603" target="_blank" class="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github">
+                        <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                        <path d="M9 18c-4.51 2-5-2-7-2" />
+                    </svg>
+                    GitHub
+                </a>
+                <a href="#" class="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down">
+                        <path d="M12 5v14" />
+                        <path d="m19 12-7 7-7-7" />
+                    </svg>
+                    Mas info
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-fix-margin">
+                <div class="title-card">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#00ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-contact-round">
+                        <path d="M16 2v2" />
+                        <path d="M17.915 22a6 6 0 0 0-12 0" />
+                        <path d="M8 2v2" />
+                        <circle cx="12" cy="12" r="4" />
+                        <rect x="3" y="4" width="18" height="18" rx="2" />
+                    </svg>
+                    <h2 class="title">Sobre mí</h2>
+                </div>
+            </div>
+            <p class="card-fix-margin">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+                Similique, odio?T empora, non consequuntur rerum at doloribus <br>
+                pariatur cumque laboriosam provident magni itaque, optio fuga? <br>
+                Suscipit possimus consectetur porro modi maiores. <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+                Similique, odio?T empora, non consequuntur rerum at doloribus <br>
+                pariatur cumque laboriosam provident magni itaque, optio fuga? <br>
+                Suscipit possimus consectetur porro modi maiores. <br>
+            </p>
+            <div class="misc-details card-fix-margin">
+                <div class="detail card-fix-margin">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-briefcase-business">
+                        <path d="M12 12h.01" />
+                        <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
+                        <path d="M22 13a18.15 18.15 0 0 1-20 0" />
+                        <rect width="20" height="14" x="2" y="6" rx="2" />
+                    </svg>
+                    <p>Disponible</p>
+                </div>
+                <div class="detail card-fix-margin">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin">
+                        <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                        <circle cx="12" cy="10" r="3" />
+                    </svg>
+                    <p>Veracruz, México</p>
+                </div>
+                <div class="detail card-fix-margin">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cake">
+                        <path d="M20 21v-8a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8" />
+                        <path d="M4 16s.5-1 2-1 2.5 2 4 2 2.5-2 4-2 2.5 2 4 2 2-1 2-1" />
+                        <path d="M2 21h20" />
+                        <path d="M7 8v3" />
+                        <path d="M12 8v3" />
+                        <path d="M17 8v3" />
+                        <path d="M7 4h.01" />
+                        <path d="M12 4h.01" />
+                        <path d="M17 4h.01" />
+                    </svg>
+                    <p>16 Dic</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="center">
+        <div class="title-card">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#00ffff" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-box">
+                <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
+                <path d="m3.3 7 8.7 5 8.7-5" />
+                <path d="M12 22V12" />
+            </svg>
+            <h1 class="title" id="proyectos">Proyectos</h1>
+        </div>
+    </div>
+
+    <div class="center">
+        <div class="card project-view">
+            <img class="thumbnail" src="https://filebeam.xyz/file/1HFOHG.png" alt="">
+            <h2 class="title">FileBeam (API)</h2>
+            <p>Página para subir compartir archivos mediante enlaces directos.
+                Permite inserción externa. La API permite subir archivos de forma externa 
+                a la página, entre otras funciones en desarrollo.
+            </p>
+            <a class="button" href="https://github.com/filebeam/api" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github">
+                    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                    <path d="M9 18c-4.51 2-5-2-7-2" />
+                </svg>
+                GitHub
+            </a>
+            <a class="button" href="https://filebeam.xyz" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-arrow-out-up-right">
+                    <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
+                    <path d="m21 3-9 9" />
+                    <path d="M15 3h6v6" />
+                </svg>
+                Visitar
+            </a>
+        </div>
+    </div>
+
+    <div class="center">
+    </div>
+
+</body>
+
+</html>
