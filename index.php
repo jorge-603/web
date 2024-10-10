fead3d8e68501de38849cf226cdba42466e97b93
Jorge603 <jorgean9999@gmail.com>
2024-10-10 01:14:11 -0600

    cambios minimos

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <ul class="menu">
             <li><a href="#">Acerca de</a></li>
             <li><a href="#proyectos">Proyectos</a></li>
-            <li><a href="#">Contacto</a></li>
+            <li><a href="#contacto">Contacto</a></li>
             <li><a href="#">Blog</a></li>
         </ul>
         </div>
@@ -124,7 +124,7 @@
             <img class="thumbnail" src="https://filebeam.xyz/file/1HFOHG.png" alt="">
             <h2 class="title">FileBeam (API)</h2>
             <p>Página para subir compartir archivos mediante enlaces directos.
-                Permite inserción externa. La API permite subir archivos de forma externa 
+                Permite inserción externa. La API permite subir archivos de forma externa
                 a la página, entre otras funciones en desarrollo.
             </p>
             <a class="button" href="https://github.com/filebeam/api" target="_blank">
@@ -146,6 +146,24 @@
     </div>
 
     <div class="center">
+        <div class="title-card">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#00ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail">
+                <rect width="20" height="16" x="2" y="4" rx="2" />
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            </svg>
+            <h1 class="title" id="contacto">Contacto</h1>
+        </div>
+    </div>
+
+    <div class="center">
+        <div class="title-card">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#00ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-user">
+                <rect width="18" height="18" x="3" y="3" rx="2" />
+                <circle cx="12" cy="10" r="3" />
+                <path d="M7 21v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2" />
+            </svg>
+            <h1 class="title">Acerca de</h1>
+        </div>
     </div>
 
 </body>
